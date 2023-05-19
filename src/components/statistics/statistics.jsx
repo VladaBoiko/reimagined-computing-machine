c5be0cc36d687eac93a7c841365f7dddd5ab90cb
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-05-19 16:47:28 +0300

    add

---
+++ src/components/statistics/statistics.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { getRandomHexColor } from './getRandomHexColor';
+import './statistics.styled.css';
+
+const Statistics = ({ stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li
+            className="item"
+            key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
